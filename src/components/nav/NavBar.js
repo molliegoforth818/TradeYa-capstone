63569b8cf1983ec7b7fbb74d80b4adfb7bfe05ad
Mollie Goforth <molliegoforth818@gmail.com>
2020-03-24 15:30:07 -0500

    navbar added and groupcloset

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+const NavBar = props => {
+  const manageLogout = () => {
+    props.clearAsUser();
+    props.history.push("/login");
+  };
+
+  return (
+    <header>
+      <h1 className="project-title">Trade Ya!</h1>
+
+      <nav>
+        <ul className="navlink-container">
+          <li>
+            <NavLink
+              className="navlink"
+              to="/closet"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "mint green"
+              }}
+            >
+              Closet
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="navlink"
+              to="/tasks"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "red"
+              }}
+            >
+              Tasks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="navlink"
+              to="/news"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "red"
+              }}
+            >
+              
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="navlink"
+              to="/users"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "mint green"
+              }}
+            >
+              Users
+            </NavLink>
+          </li>
+          {!props.currentUser ? (
+            <li>
+              <NavLink
+                className="navlink"
+                to="/login"
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "mint green"
+                }}
+              >
+                Login
+              </NavLink>
+            </li>
+          ) : (
+            <li>
+              <span className="navlink" onClick={manageLogout}>
+                Logout
+              </span>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default withRouter(NavBar);
