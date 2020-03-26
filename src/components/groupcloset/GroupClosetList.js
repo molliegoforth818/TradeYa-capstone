755a44e1a9ff2fbf85605904ada56f79faafbdcd
Mollie Goforth <molliegoforth818@gmail.com>
2020-03-26 13:08:42 -0500

    commit

--- src/components/groupcloset/GroupClosetList.js
+++ src/components/groupcloset/GroupClosetList.js
@@ -1,34 +1,34 @@
-// import React, { useEffect, useState } from "react";
-// import GroupClosetCard from "./ClothingItemCard";
-// import ClosetManager from "../../modules/ClosetManager";
+import React, { useEffect, useState } from "react";
+import GroupClosetCard from "./ClothingItemCard";
+import GroupClosetManager from "../../modules/GroupClosetManager";
 
-// const GroupClosetList = props => {
-//   const [clothingItem, setClothingItem] = useState([]);
+const GroupClosetList = props => {
+  const [clothingLend, setClothingLend] = useState([]);
   
-//   const getClothingItem = () => {
-//     return ClosetManager.getAllClothingItemsByUser().then(clothingItemsFromAPI => {
-//         const userClothingItem = clothingItemsFromAPI.filter(
-//             clothingItem =>clothingItem.user.id === userNow
-//         );
-//         setClothingItem(userClothingItem)
-//     });
-// };
-// useEffect(() => {
-//     getClothingItem();
-// }, []);
-//   return (
-//     <React.Fragment>      
-//       <div className="container-closetCards">
-//         {groupCloset.map(groupCloset => (
-//           <GroupClosetCard
-//             key={groupCloset.id}
-//             groupCloset={groupCloset}
-//             getClothingItem={getClothingItem}
-//             {...props}
-//           />
-//         ))}
-//       </div>
-//     </React.Fragment>
-//   );
-// };
-// export default GroupClosetList;
+  const getClothingItem = () => {
+    return ClosetManager.getAllClothingItemsByUser().then(clothingItemsFromAPI => {
+        const userClothingItem = clothingItemsFromAPI.filter(
+            clothingItem =>clothingItem.user.id === userNow
+        );
+        setClothingItem(userClothingItem)
+    });
+};
+useEffect(() => {
+    getClothingItem();
+}, []);
+  return (
+    <React.Fragment>      
+      <div className="container-closetCards">
+        {groupCloset.map(groupCloset => (
+          <GroupClosetCard
+            key={groupCloset.id}
+            groupCloset={groupCloset}
+            getClothingItem={getClothingItem}
+            {...props}
+          />
+        ))}
+      </div>
+    </React.Fragment>
+  );
+};
+export default GroupClosetList;
